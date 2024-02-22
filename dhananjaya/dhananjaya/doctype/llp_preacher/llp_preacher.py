b19298dc52b02b35959afcd20a2213f27530c247
hkmj-erp <it@harekrishnajaipur.org>
2024-02-22 16:06:28 +0530

    misc

--- dhananjaya/dhananjaya/doctype/llp_preacher/llp_preacher.py
+++ dhananjaya/dhananjaya/doctype/llp_preacher/llp_preacher.py
@@ -9,7 +9,6 @@ PREACHER_ROLE = "DCC Preacher"
 
 class LLPPreacher(Document):
     def on_update(self):
-        frappe.errprint("Preacher Updated2")
         for user_row in self.allowed_users:
             if PREACHER_ROLE not in frappe.get_roles(user_row.user):
                 user = frappe.get_doc("User", user_row.user)
